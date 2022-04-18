a18f5b1c10e36a36610975561449049c49a7e2cf
Kaushal Jhunjhunwala <40262851+kaushaljjw206@users.noreply.github.com>
2022-04-18 11:42:07 +0530

    Create campingListController.js

---
+++ Lightning Components/Input Form with Events/campingListController.js
@@ -0,0 +1,48 @@
+({
+	doInit : function(component, event, helper) {
+		component.set("v.CampingColumn",[
+                      {label: 'Camping Item Id', fieldName: 'Id', type: 'text'},
+                      {label: 'Camping Item Name', fieldName: 'Name', type: 'text'},
+                      {label: 'Packed', fieldName: 'Packed__c', type: 'text'},
+                      {label: 'Price', fieldName: 'Price__c', type: 'number'},
+                      {label: 'Quantity', fieldName: 'Quantity__c', type: 'number'},
+                     ]);
+            component.set("v.items",[{
+            Id: "Id",
+            Name : "Name",
+            Packed__c : "Packed__c",
+            Price__c : "Price__c",
+            Quantity__c : "Quantity"
+            }]);
+        let action = component.get('c.getItems'); //create action
+            action.setCallback(this,function(response){ //callback  function
+            	let state = response.getState();
+            	let CampingValue = response.getReturnValue();
+            if(state === 'SUCCESS'){
+            	component.set("v.items", CampingValue);
+            }
+            });
+            $A.enqueueAction(action); //queue action
+	},
+            
+    //callcontroller : function(component, event, helper) {
+    	//let newCamping = component.get('v.CampingCreate');
+        //helper.createItem(component, newCamping);  //calling helper method
+    handleAddItem : function(component, event, helper){
+        let eventValue = event.get("item");
+        let action = component.get("c.saveItem");
+        action.setParams({
+            "ci" : eventItem
+        });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (component.isValid() && state === "SUCCESS") {
+                var items = component.get("v.items");
+                items.push(item);
+                component.set("v.items",items);
+            }
+        });
+        $A.enqueueAction(action);
+    }
+    
+})
